08e541b7717ae5ecf9c843b6954b24d62220718b
Rique2 <enrique2heinrich@gmail.com>
2023-04-10 09:47:04 +0200

    Add Inputs / Labels

--- src/App.js
+++ src/App.js
@@ -7,18 +7,36 @@ class App extends Component{
 
 
 render(){
+
   return(
     <div className='cvContainer'>
       <div className='cvHeader'>
         CV-Container
       </div>
-      <div className='Info'>
-        <div>General Information</div>
+      <div className='info'>
+      <form>
+        <div id='generalInfo'>
+          <h4>General Information</h4>
         <input type={'text'} id='taskInput' placeholder='First Name'/>
         <input type={'text'} id='taskInput' placeholder='Last Name'/>
         <input type={'email'} id='taskInput' placeholder='Email'/>
         <input type={'number'} id='taskInput' placeholder='Phone Number'/>
+        </div>
+        <div id='experience'>
+          <h4>Experience</h4>
+          <input type={'text'} id='taskInput' placeholder='School Name' />
+          <input type={'text'} id='taskInput' placeholder='Title of Study' />
+          <label className='start'/>
+            <p>Start</p>
+            <input type={'date'} id='taskInput'/>
+          <label className='end'>
+            <p>End</p>
+            <input type={'date'} id='taskInput'/>
+          </label>
+        </div>
+        </form>
       </div>
+     
     </div>
   )
 }
