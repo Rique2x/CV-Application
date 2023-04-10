471f011373a30f51b3eb005c346648b0e40df1b0
Rique2 <enrique2heinrich@gmail.com>
2023-04-10 15:27:24 +0200

    moved the cv resume in a new file

--- src/App.js
+++ src/App.js
@@ -1,126 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Home from './components/home';
+import Resume from './components/home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component{
-  constructor(props){
-    super(props)
 
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-    };  
-  }
 
-  handleNameChange = (e) => {
-    this.setState({
-      name: e.target.value
-    });
-  }
-
-  handleEmailChange = (e) => {
-    this.setState({
-      email: e.target.value
-    });
-  }
-
-  handlePhoneChange = (e) => {
-    this.setState({
-      phone: e.target.value
-    });
-  }
-
-  handleDateChange = (e) => {
-    this.setState({
-      date: e.target.value
-    });
-  }
-
-render(){
-  const { name, email, phone, date } = this.state;
-
-  return(
-
-    <div className='cvContainer'>
-
-      <div className='cvHeader'>
-        CV-Container
-      </div>
-
-      <div className='info'>
-
-      <form>
-        <div id='generalInfo'>
-          <h4>General Information</h4>
-        <input 
-        onChange={this.handleNameChange}
-        value={name} 
-        type={'text'} 
-        id='taskInput' 
-        placeholder='First Name'
-        />
-        <input 
-        onChange={this.handleNameChange}
-        value={name} 
-        type={'text'} 
-        id='taskInput' 
-        placeholder='Last Name'
-        />
-        <input 
-        onChange={this.handleEmailChange}
-        value={email} 
-        type={'email'} 
-        id='taskInput' 
-        placeholder='Email'
-        />
-        <input 
-        onChange={this.handlePhoneChange}
-        value={phone} 
-        type={'tel'} 
-        id='taskInput' 
-        placeholder='Phone Number'
-        />
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <h1>CV App</h1>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+          </Switch>
         </div>
-
-        <div id='experience'>
-          <h4>Experience</h4>
-          <input 
-          onChange={this.handleNameChange}
-          value={name}
-          type="text"
-          id='taskInput' 
-          placeholder='School Name' 
-          />
-          <input 
-          onChange={this.handleNameChange}
-          value={name} 
-          type={'text'} 
-          id='taskInput' 
-          placeholder='Title of Study' 
-          />
-          <label className='start'/>
-            <p>Start</p>
-            <input 
-            onChange={this.handleNameChange}
-            value={date} 
-            type={'date'} 
-            id='taskInput'
-            />
-          <label className='end'>
-            <p>End</p>
-            <input 
-            onChange={this.handleDateChange}
-            value={date} 
-            type={'date'} 
-            id='taskInput'
-            />
-          </label>
-        </div>
-        </form>
-      </div>
-     
-    </div>
-  )
-}
+      </Router>
+    );
+  }
 }
 
 export default App;
