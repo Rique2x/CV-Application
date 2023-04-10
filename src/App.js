e2214d14be11f8844621d3ad69fcdd1c3a64e90e
Rique2 <enrique2heinrich@gmail.com>
2023-04-10 11:13:30 +0200

    add value attributes of the input element

--- src/App.js
+++ src/App.js
@@ -17,18 +17,22 @@ render(){
   const { name, email, phone } = this.state;
 
   return(
+    
     <div className='cvContainer'>
+
       <div className='cvHeader'>
         CV-Container
       </div>
+
       <div className='info'>
+
       <form onSubmit={handleSubmit}>
         <div id='generalInfo'>
           <h4>General Information</h4>
-        <input type={'text'} id='taskInput' placeholder='First Name'/>
-        <input type={'text'} id='taskInput' placeholder='Last Name'/>
-        <input type={'email'} id='taskInput' placeholder='Email'/>
-        <input type={'tel'} id='taskInput' placeholder='Phone Number'/>
+        <input value={name} type={'text'} id='taskInput' placeholder='First Name'/>
+        <input value={name} type={'text'} id='taskInput' placeholder='Last Name'/>
+        <input value={email} type={'email'} id='taskInput' placeholder='Email'/>
+        <input value={phone} type={'tel'} id='taskInput' placeholder='Phone Number'/>
         </div>
         <div id='experience'>
           <h4>Experience</h4>
